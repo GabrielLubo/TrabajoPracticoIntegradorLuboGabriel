b533971b999535ba75eda4cec1b5845b9a2d0302
GabrielLubo <68754915+GabrielLubo@users.noreply.github.com>
2023-06-11 23:36:58 -0300

    agrego js

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ function calcularTotal() {
         entradaTrainee,
         entradaJr,
         general;
-    
+
     console.log('funcion');
     cantEntradas = document.getElementById('inputCantidad').value;
     categoria = document.getElementById('inputCategoria').value;
@@ -18,33 +18,33 @@ function calcularTotal() {
     entradaJr = 200 - (200 * 15) / 100;
     general = 200;
 
-    if (cantEntradas > 0 && !isNaN(cantEntradas && cantEntradas % 1 == 0)){
-        if(categoria == 'Estudiante') {
+    if (cantEntradas > 0 && !isNaN(cantEntradas && cantEntradas % 1 == 0)) {
+        if (categoria == 'Estudiante') {
             total = entradaEstudiante * cantEntradas;
             document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024' + total;
         }
-        if(categoria == 'Trainee'){
+        if (categoria == 'Trainee') {
             total = entradaTrainee * cantEntradas;
             document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024' + total;
         }
-        if(categoria == 'Junior'){
+        if (categoria == 'Junior') {
             total = entradaJr * cantEntradas;
             document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024' + total;
         }
-        if(categoria == 'General'){
+        if (categoria == 'General') {
             total = general * cantEntradas;
             document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024' + total;
         }
-    }else{
+    } else {
         window.alert('La cantidad de entradas debe ser en números enteros');
     }
-    }
+}
 
-    function borrar() {
-        document.getElementById('inputNombre').value = ' ';
-        document.getElementById('inputApellido').value = ' ';
-        document.getElementById('inputEmail').value = ' ';
-        document.getElementById('inputCantidad').value = ' ';
-        document.getElementById('inputCategoria').value = 'Estudiante';
-        document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024';
-      }
+function borrar() {
+    document.getElementById('inputNombre').value = ' ';
+    document.getElementById('inputApellido').value = ' ';
+    document.getElementById('inputEmail').value = ' ';
+    document.getElementById('inputCantidad').value = ' ';
+    document.getElementById('inputCategoria').value = 'Estudiante';
+    document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024';
+}
